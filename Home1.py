8c306e95ae10d0bb62b7258b6badcc38bdbfd60a
Denis <denis.300598@mail.ru>
2022-10-09 22:21:00 +0600

    add все значения предикат

--- Home1.py
+++ Home1.py
@@ -56,12 +56,51 @@
 # A (3,6); B (2,1) -> 5,09
 # A (7,-5); B (1,-1) -> 7,21
 
-print('Enter coordinates point А:')
-x_A = float(input('X: '))
-y_A = float(input('Y: '))
-print("Enter coordinates point B:")
-x_B = float(input('X: '))
-y_B = float(input('Y: '))
+#print('Enter coordinates point А:')
+#x_A = float(input('X: '))
+#y_A = float(input('Y: '))
+#print("Enter coordinates point B:")
+#x_B = float(input('X: '))
+#y_B = float(input('Y: '))
 
-from math import sqrt
-print('Distance between A and B: ',round(sqrt((x_A - x_B)**2 + (y_A - y_B)**2), 2)) # Теорема Пифагора
+#from math import sqrt
+#print('Distance between A and B: ',round(sqrt((x_A - x_B)**2 + (y_A - y_B)**2), 2)) # Теорема Пифагора
+
+
+# Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+#x = int(input('Введите число x '))
+#y = int(input('Введите число y '))
+#z = int(input('Введите число z '))
+
+#a = x * y * z
+#b = x + y + z
+
+#if a > 0:
+#   a = 0
+#elif a < 1:
+#    a = 1
+#if b > 0:
+#   b = 1
+#elif b < 1:
+#   b = 1
+
+#if a == b:
+#   print('Утверждение истинно')
+#elif a != b:
+#   print('Утверждение ложно')
+
+#leftSide = not (x or y or z)
+#rightSide = not x and not y and not z
+#result = leftSide == rightSide
+#if result == True:
+#   print('Утверждение истинно')
+#else:
+#    print('Утверждение ложно')
+
+
+#Или такой вариант решения
+for x in range(2):
+        for y in range(2):
+            for z in range(2):
+                print(not (x or y or z) == (not x and not y and not z))
+                print(x, y, z)
