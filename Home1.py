608a919e87e5dba924ec66a5ba8418447357965b
Denis <denis.300598@mail.ru>
2022-10-09 21:59:20 +0600

    add нахождение расстояния между координатами

--- Home1.py
+++ Home1.py
@@ -37,14 +37,31 @@
 # 3. Напишите программу, которая по заданному номеру четверти, 
 # показывает диапазон возможных координат точек в этой четверти (x и y).
 
-n = int(input('input quarter number: '))
-if n < 1 or n > 4:
-    print('Please, repeat the input')
-elif n == 1:
-    print('x > 0 and y > 0')
-elif n == 2:
-    print('x < 0 and y > 0')
-elif n == 3:
-    print('x < 0 and y < 0')
-elif n == 4:
-    print('x > 0 and y < 0')
+#n = int(input('input quarter number: '))
+#if n < 1 or n > 4:
+#    print('Please, repeat the input')
+#elif n == 1:
+#    print('x > 0 and y > 0')
+#elif n == 2:
+#   print('x < 0 and y > 0')
+#elif n == 3:
+#    print('x < 0 and y < 0')
+#elif n == 4:
+#    print('x > 0 and y < 0')
+
+
+# 4. Напишите программу, которая принимает на вход координаты двух точек 
+# и находит расстояние между ними в 2D пространстве.
+# Пример:
+# A (3,6); B (2,1) -> 5,09
+# A (7,-5); B (1,-1) -> 7,21
+
+print('Enter coordinates point А:')
+x_A = float(input('X: '))
+y_A = float(input('Y: '))
+print("Enter coordinates point B:")
+x_B = float(input('X: '))
+y_B = float(input('Y: '))
+
+from math import sqrt
+print('Distance between A and B: ',round(sqrt((x_A - x_B)**2 + (y_A - y_B)**2), 2)) # Теорема Пифагора
