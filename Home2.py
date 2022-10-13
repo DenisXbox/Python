7d5a9ac8668456399ca2634cc2a9eae8707eba69
Denis <denis.300598@mail.ru>
2022-10-13 22:34:38 +0600

    add Задача 3

--- Home2.py
+++ Home2.py
@@ -21,3 +21,16 @@
 #for i in range(1, n+1):
 #    factorial *= i
 #print(factorial, end=' ')
+
+
+# 3.Задайте список из n чисел 
+# последовательности $(1+\frac 1 n)^n$ и выведите на экран их сумму.
+
+n = int(input('Enter number: ')) 
+
+def sequence(n):
+
+    return[round((1 + 1 / x)**x, 2) for x in range (1, n + 1)]   
+        
+print(sequence(n))
+print(round(sum(sequence(n))))
